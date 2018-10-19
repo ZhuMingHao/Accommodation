dec8eb9dd9e73e1664c9b35440801a2307e76268
Nico Zhu <v-zhumin@microsoft.com>
2018-10-19 17:38:59 +0800

    Add project files.

---
+++ Accommodation/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accommodation
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public int TimeDuration { get; set; }
+        public int Proportion { get; set; }
+    }
+}
